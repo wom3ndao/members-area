51e9e656fcb61c73916ef642386e79a396a22e1d
rike <hello@rike.dev>
2023-06-11 00:26:12 +0200

    improve admin

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,14 +1,16 @@
 import { get_stage } from "@/utils";
 import { ethers, Signer } from "ethers";
-import { useAccount, useSigner } from "wagmi";
+import { useAccount, useProvider, useSigner } from "wagmi";
 import useNfts, { Token } from "../hooks/useNfts";
 import { constants } from "../constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Admin() {
   const { address } = useAccount();
   const { data: signer } = useSigner();
+  const provider = useProvider();
   const [vault, setVault] = useState<any>();
+  const [vaultNow, setVaultNow] = useState<string>();
   const contract = new ethers.Contract(
     get_stage() === "production"
       ? constants.NFT_MEMBERSHIP_ADDRESS_Q_MAINNET
@@ -16,26 +18,55 @@ export default function Admin() {
     constants.NFT_MEMBERSHIP_ABI
   );
 
+  useEffect(() => {
+    getVaultAddress();
+  }, []);
+
+  const getVaultAddress = async () => {
+    const vAddress = await contract.connect(provider).vaultAddress();
+    setVaultNow(vAddress);
+  };
+
   const setVaultTo = async () => {
-    if (!address) return;
+    if (!address && !vault) return;
     try {
       const tx = await contract
         .connect(signer as Signer)
-        .setVaultAddress(vault);
+        .setVaultAddress("0x8626143c513639cB09Fa4a6Ed1420D86d65c59fC", {
+          gasLimit: 300000,
+        });
       const result = await tx.wait();
       console.log(result);
     } catch (e) {
       console.log(e);
     }
   };
+
+  const setTransferToTrue = async () => {
+    try {
+      const tx = await contract
+        .connect(signer as Signer)
+        .setTransferAllowed(true, {
+          gasLimit: 300000,
+        });
+      const result = await tx.wait();
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="bg-white shadow sm:rounded-lg">
-      <div className="px-4 py-5 sm:p-6">
+    <div className="bg-white shadow sm:rounded-lg p-6">
+      <div className="pb-8">
         <h3 className="text-base font-semibold leading-6 text-gray-900">
-          Update your email
+          DAO Vault Address
         </h3>
         <div className="mt-2 max-w-xl text-sm text-gray-500">
-          <p>Change the email address you want associated with your account.</p>
+          <p>
+            <b>now set to {vaultNow}</b>
+          </p>
+          <p>Change the DAO vault address you want associated with your DAO.</p>
         </div>
         <div className="mt-5 sm:flex sm:items-center">
           <div className="w-full sm:max-w-xs">
@@ -57,6 +88,34 @@ export default function Admin() {
           >
             Save
           </button>
+        </div>
+      </div>
+      <hr />
+      <div className="pt-6 pb-6">
+        <button
+          onClick={() => setTransferToTrue()}
+          type="submit"
+          className="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto"
+        >
+          Set Transfer to True
+        </button>
+      </div>
+      <hr />
+      <div className="pt-6 pb-6">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">
+          DAO Address
+        </h3>
+        <div className="mt-2 max-w-xl text-sm text-gray-500">
+          <p>now set to 0xE53D6986B2d7dE9AA9E65cf90F427Cf964592914</p>
+        </div>
+      </div>
+      <hr />
+      <div className="pt-6 pb-6">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">
+          NFT Address
+        </h3>
+        <div className="mt-2 max-w-xl text-sm text-gray-500">
+          <p>now set to {constants.NFT_MEMBERSHIP_ADDRESS_Q_TESTNET}</p>
         </div>
       </div>
     </div>
