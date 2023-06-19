48a1d9242039f57da3b43eabf17392eb4b4a30a7
rike <hello@rike.dev>
2023-06-20 01:03:43 +0200

    init light theme

--- src/navigation/Header/components/Settings/components/SettingsMenu/SettingsMenu.tsx
+++ src/navigation/Header/components/Settings/components/SettingsMenu/SettingsMenu.tsx
@@ -1,42 +1,36 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
-import { Switch } from '@q-dev/q-ui-kit';
-import { useLanguage } from 'context/LanguageProvider';
-import { useTheme } from 'styled-components';
+import { Switch } from "@q-dev/q-ui-kit";
+import { useLanguage } from "context/LanguageProvider";
+import { useTheme } from "styled-components";
 
-import { SettingsMenuContainer } from './styles';
+import { SettingsMenuContainer } from "./styles";
+import { useEffect } from "react";
 
-function LanguageSwitcher ({ onLanguageOpen }: { onLanguageOpen: () => void }) {
+function LanguageSwitcher({ onLanguageOpen }: { onLanguageOpen: () => void }) {
   const { isDarkTheme, onChangeTheme } = useTheme();
 
   const { i18n, t } = useTranslation();
   const { languageList } = useLanguage();
   const language = languageList.find(({ locale }) => locale === i18n.language) || languageList[0];
 
+  useEffect(() => {
+    if (isDarkTheme) onChangeTheme();
+  }, []);
+
   return (
     <SettingsMenuContainer>
       <div className="language-block" onClick={onLanguageOpen}>
-        <p className="text-lg">{t('LANGUAGE')}</p>
+        <p className="text-lg">{t("LANGUAGE")}</p>
 
         <div className="language-pick">
-          <img
-            className="language-pick__flag"
-            src={language.flagSrc}
-            alt="flag"
-          />
+          <img className="language-pick__flag" src={language.flagSrc} alt="flag" />
 
-          <p className="text-md font-semibold">
-            {language.language}
-          </p>
+          <p className="text-md font-semibold">{language.language}</p>
         </div>
       </div>
 
-      <Switch
-        className="theme-toggle"
-        label={t('DARK_THEME')}
-        value={isDarkTheme}
-        onChange={onChangeTheme}
-      />
+      <Switch className="theme-toggle" label={t("DARK_THEME")} value={isDarkTheme} onChange={onChangeTheme} />
     </SettingsMenuContainer>
   );
 }
